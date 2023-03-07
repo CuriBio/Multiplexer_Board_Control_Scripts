b16b95823d94ff52de0e95e79c2c36e8204d05a7
kg-curi <86029660+kg-curi@users.noreply.github.com>
2023-03-06 18:28:29 -0800

    All wells operational

--- Stim_Lid_Connection_Tester/Connection_Tester/Connection_Tester.ino
+++ Stim_Lid_Connection_Tester/Connection_Tester/Connection_Tester.ino
@@ -1,6 +1,6 @@
 
-int WELL_NUMBER = 16;
-int STARTING_PIN = 22;
+int WELL_NUMBER = 24;
+int STARTING_PIN = 2;
 
 String rows[] = {"A", "B", "C", "D"};
 String cols[] = {"1", "2", "3", "4", "5", "6"};
@@ -17,22 +17,22 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     Serial.read();
-    String connections[48];
-    String ideals[48];
+    String connections[WELL_NUMBER*2];
+    String ideals[WELL_NUMBER*2];
 
-    for (int test_pin = 0; test_pin < 16; test_pin++) {
+    for (int test_pin = 0; test_pin < WELL_NUMBER; test_pin++) {
       connections[test_pin] = rows[test_pin / 6] + cols[test_pin % 6] + pol[0];
       ideals[test_pin] = rows[test_pin / 6] + cols[test_pin % 6] + pol[0] + rows[test_pin / 6] + cols[test_pin % 6] + pol[1];
       pinMode(STARTING_PIN + 2*test_pin, OUTPUT);
       digitalWrite(STARTING_PIN + 2*test_pin, 0);
 
-      for (int well = 0; well < 16; well++) { // iterate through negative pins
+      for (int well = 0; well < WELL_NUMBER; well++) { // iterate through negative pins
         if (!digitalRead(STARTING_PIN + 2 * well) && well != test_pin) { 
           connections[test_pin].concat(rows[well / 6] + cols[well % 6] + pol[0]);          
         }
       }
 
-      for (int well = 0; well < 16; well++) { // iterate through negative pins
+      for (int well = 0; well < WELL_NUMBER; well++) { // iterate through negative pins
         if (!digitalRead(STARTING_PIN + 2 * well + 1)) { 
           connections[test_pin].concat(rows[well / 6] + cols[well % 6] + pol[1]);          
         }
@@ -40,19 +40,19 @@ void loop() {
       pinMode(STARTING_PIN + 2*test_pin, INPUT_PULLUP);
     }
 
-    for (int test_pin = 0; test_pin < 16; test_pin++) {
+    for (int test_pin = 0; test_pin < WELL_NUMBER; test_pin++) {
       connections[test_pin + WELL_NUMBER] = rows[test_pin / 6] + cols[test_pin % 6] + pol[1];
       ideals[test_pin + WELL_NUMBER] = rows[test_pin / 6] + cols[test_pin % 6] + pol[1] + rows[test_pin / 6] + cols[test_pin % 6] + pol[0];
       pinMode(STARTING_PIN + 2*test_pin + 1, OUTPUT);
       digitalWrite(STARTING_PIN + 2*test_pin + 1, 0);
 
-      for (int well = 0; well < 16; well++) { // iterate through negative pins
+      for (int well = 0; well < WELL_NUMBER; well++) { // iterate through negative pins
         if (!digitalRead(STARTING_PIN + 2 * well + 1) && well != test_pin) { 
           connections[test_pin + WELL_NUMBER].concat(rows[well / 6] + cols[well % 6] + pol[1]);          
         }
       }
 
-      for (int well = 0; well < 16; well++) { // iterate through negative pins
+      for (int well = 0; well < WELL_NUMBER; well++) { // iterate through negative pins
         if (!digitalRead(STARTING_PIN + 2 * well)) { 
           connections[test_pin + WELL_NUMBER].concat(rows[well / 6] + cols[well % 6] + pol[0]);          
         }
@@ -62,7 +62,8 @@ void loop() {
     }
 
     bool same = true; 
-    Serial.println("Open/Inter-channel short");
+    Serial.println("Begin Test");
+    Serial.println();
     for (int i = 0; i < 2 * WELL_NUMBER; i++) {
       // Serial.println(ideals[i]);
       // Serial.println(connections[i]);
@@ -79,10 +80,13 @@ void loop() {
         Serial.print(ideals[i] + " are connected");        
       }
       Serial.println();
+            
     }
     if (same) {
-      Serial.println()
+      Serial.println();
       Serial.println("Connections Good");
-    } 
+
+    }
+    Serial.println();
   }
 }
